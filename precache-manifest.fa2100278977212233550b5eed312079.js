b53c06d6f71c26b319b616412256f8bf4f7da82f
hitbug9 <kenichi.kanamori9@gmail.com>
2022-10-30 19:38:37 -0700

    Updates

---
+++ precache-manifest.fa2100278977212233550b5eed312079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ba4662f61a440ed2832af9a1ce72ce",
+    "url": "/kenichi-kanamori/index.html"
+  },
+  {
+    "revision": "4db28736a09b7d3f1ad0",
+    "url": "/kenichi-kanamori/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "aa29a6ac37aef017c26b",
+    "url": "/kenichi-kanamori/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "4db28736a09b7d3f1ad0",
+    "url": "/kenichi-kanamori/static/js/2.ddc1ac24.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/kenichi-kanamori/static/js/2.ddc1ac24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa29a6ac37aef017c26b",
+    "url": "/kenichi-kanamori/static/js/main.1b426020.chunk.js"
+  },
+  {
+    "revision": "a930a9a21eee91b44e16",
+    "url": "/kenichi-kanamori/static/js/runtime-main.1b018149.js"
+  }
+]);
